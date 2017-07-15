c50758ded927200b54a338245c892ba069d364b3
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-07-14 21:15:47 -0400

    added non recursive solution

--- src/org/programming/algorithms/math/Fibonacci.java
+++ src/org/programming/algorithms/math/Fibonacci.java
@@ -5,6 +5,8 @@ public class Fibonacci {
 	
 	// recursive solution 
 	// problem with this solution is the stack/heap the recursion requires for large numbers
+	// complexity O(n^2)
+	// using this to compute the fibonacci number of 45 is slow in comparison the other two non recursive algorithms below
 	static public long fibonacci(long num) {
 		
 		return num < 2 ? num : fibonacci (num - 1) + fibonacci (num - 2);		
@@ -12,6 +14,7 @@ public class Fibonacci {
 	
 	// non recursive solution using a vector to keep track of the previous terms
 	// problem is the amount of memory this solution uses
+	// complexity O(n)
 	static public long fibonacci2(long num) {
 
 		long[] numbers = new long[(int)num+2];
@@ -24,6 +27,27 @@ public class Fibonacci {
 		}
 		
 		return numbers[(int)num];
+	}
+	
+	
+	// 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 
+	// non recursive solution the keeps track of the last two fibonacci numbers and uses them
+	// to calculate the next one.
+	// complexity O(n)
+	static public long fibonacci3(long num) {
+		if (num < 2) {
+			return num;
+		}
+		long minus1 = 1;
+		long minus2 = 0;
+		long result = minus1 + minus2;
+		for (int i = 2; i <= num; i++) {
+			result = minus1 + minus2;
+			minus2 = minus1;
+			minus1 = result;
+		}
+		
+		return result;
 	}
 
 }
--- src/org/programming/algorithms/math/FibonacciTest.java
+++ src/org/programming/algorithms/math/FibonacciTest.java
@@ -10,46 +10,59 @@ public class FibonacciTest {
 	public void testOne() {
 		assertEquals(1, Fibonacci.fibonacci(1));
 		assertEquals(1, Fibonacci.fibonacci2(1));
+		assertEquals(1, Fibonacci.fibonacci3(1));
 	}
 	
 	@Test
 	public void testTwo() {
 		assertEquals(1, Fibonacci.fibonacci(2));
 		assertEquals(1, Fibonacci.fibonacci2(2));
+		assertEquals(1, Fibonacci.fibonacci3(2));
 	}
 	
 	@Test
 	public void testThree() {
 		assertEquals(2, Fibonacci.fibonacci(3));
 		assertEquals(2, Fibonacci.fibonacci2(3));
+		assertEquals(2, Fibonacci.fibonacci3(3));		
 	}
 	
 	
 	@Test
 	public void testFour() {
 		assertEquals(3, Fibonacci.fibonacci(4));
-		assertEquals(3, Fibonacci.fibonacci2(4));		
+		assertEquals(3, Fibonacci.fibonacci2(4));	
+		assertEquals(3, Fibonacci.fibonacci3(4));		
 	}
 	
 	@Test
 	public void testFive() {
 		assertEquals(5, Fibonacci.fibonacci(5));
 		assertEquals(5, Fibonacci.fibonacci2(5));		
+		assertEquals(5, Fibonacci.fibonacci3(5));		
 	}
 	
 	@Test
 	public void testSix() {
 		assertEquals(8, Fibonacci.fibonacci(6));
 		assertEquals(8, Fibonacci.fibonacci2(6));		
+		assertEquals(8, Fibonacci.fibonacci3(6));		
 	}
 	
 	@Test
 	public void testPrint() {
 		
 		for (int i = 0; i < 15; i++ ) {
-			System.out.print(Fibonacci.fibonacci(i) + " ");
+
+			System.out.print("Fibonacci(" + i + ") = ");
+			System.out.print(Fibonacci.fibonacci2(i) + " ");
+			System.out.print(Fibonacci.fibonacci3(i) + " ");
+			System.out.println(Fibonacci.fibonacci(i) + " ");
+			
+			assertEquals(Fibonacci.fibonacci2(i), Fibonacci.fibonacci3(i));
 			
 			assertEquals(Fibonacci.fibonacci(i), Fibonacci.fibonacci2(i));
+
 		}
 		
 		System.out.println();
